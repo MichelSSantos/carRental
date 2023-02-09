5c3802123c80cdacf71891cbefc3246148358ee2
MichelSSantos <michelshaiene2010@hotmail.com>
2023-02-09 13:35:22 -0300

    Update main Program

--- src/application/ProgramCarRental.java
+++ src/application/ProgramCarRental.java
@@ -1,10 +1,36 @@
 package application;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+
 public class ProgramCarRental {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		
+		System.out.println("Entre com os dados do aluguel ");
+		System.out.println("Modelo do carro:  ");
+		String carModel = sc.nextLine();
+		System.out.println("Retirada (dd/MM/yyyy): ");
+		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+		System.out.println("Retorno (dd/MM/yyyy): ");
+		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+	
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		
+		
+		
+		sc.close();
+		
 	}
 
 }
